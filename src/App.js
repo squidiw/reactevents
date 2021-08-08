65e20fc10fb9afd3e5ed32f484bd9c060a688d43
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-08-08 14:33:19 +0000

    Improved Event Handlers with a custom argument

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ export default class App extends Component{
       }
   }
 
-  handleEvent = (event) => {
+  handleEvent = (event, newTheme) => {
       this.setState({ counter: this.state.counter + 1,
-          theme: event.target.innerText === "Normal" ? "primary" : "danger"
+          theme: newTheme
           },
           () => this.setState({ message: `${event.type}: ${this.state.counter}`}))
   }
@@ -26,10 +26,10 @@ export default class App extends Component{
 
               <div className="text-center">
                     <button className="btn btn-primary"
-                     onClick={ this.handleEvent}
+                     onClick={ (e) => this.handleEvent(e, 'primary')}
                     >Normal</button>
                   <button className="btn btn-danger m-1"
-                          onClick={ this.handleEvent}
+                          onClick={ (e) => this.handleEvent(e, 'danger')}
                   >Danger</button>
               </div>
           </div>
