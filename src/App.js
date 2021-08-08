3fdda54e4662f71f2c2f249c295775619faffe76
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-08-08 14:23:02 +0000

    Invoking Event Handlers with a custom argument

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  constructor(props) {
+      super(props);
+      this.state = {
+          message: "Ready",
+          counter : 0,
+          theme: "secondary"
+      }
+  }
+
+  handleEvent = (event) => {
+      this.setState({ counter: this.state.counter + 1,
+          theme: event.target.innerText === "Normal" ? "primary" : "danger"
+          },
+          () => this.setState({ message: `${event.type}: ${this.state.counter}`}))
+  }
+
+  render(){
+      return (
+          <div className="m-2">
+              <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
+                  {this.state.message}
+              </div>
+
+              <div className="text-center">
+                    <button className="btn btn-primary"
+                     onClick={ this.handleEvent}
+                    >Normal</button>
+                  <button className="btn btn-danger m-1"
+                          onClick={ this.handleEvent}
+                  >Danger</button>
+              </div>
+          </div>
+      )
+  }
+
 }
-
-export default App;
